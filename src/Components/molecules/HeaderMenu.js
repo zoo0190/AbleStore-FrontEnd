c0e9d5d0b42b5a15f82610e747632cc29f9cce5d
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2021-01-15 11:15:10 +0900

    Add: main header

---
+++ src/Components/molecules/HeaderMenu.js
@@ -0,0 +1,38 @@
+import React from "react";
+import HeaderButton from "../atoms/HeaderButton";
+import { Menu, Dropdown } from "antd";
+import "antd/dist/antd.css";
+import styled from "styled-components";
+
+const HeaderMenu = () => {
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <a href="#">123</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="#">456</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a href="#">789</a>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <MenuItems>
+      <Dropdown overlay={menu} trigger={["click"]}>
+        <a style={{ color: "white", padding: "30px 15px" }}>Categories</a>
+      </Dropdown>
+      <HeaderButton title="Blog" />
+      <HeaderButton title="Support" />
+    </MenuItems>
+  );
+};
+
+export default HeaderMenu;
+
+const MenuItems = styled.div`
+  display: flex;
+  align-items: center;
+`;
