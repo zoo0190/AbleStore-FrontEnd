5548a1dad3584957f3cc3e8f86536d9089e1d88b
aramcho206 <aramcho206@gmail.com>
2021-01-21 17:39:55 +0900

    chang

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import Login from "./Pages/Login/Login";
-import SignUp from "./Pages/SignUp/SignUp";
+import SignUp from "./Pages/SignUp/Signup.js";
 
 import Footer from "./Components/Organisms/Footer/Footer";
 class Routes extends React.Component {
@@ -12,7 +12,7 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/Login" component={Login} />
-          <Route exact path="/Signup" component={SignUp} />
+          <Route exact path="/SignUp" component={SignUp} />
         </Switch>
         <Footer />
       </Router>
